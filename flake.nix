60d983eba10ed7d07526227d443e7bd0158f37e4
Darsh yadav <darsh.yadav@proton.me>
2026-01-02 17:54:19 +0530

    got it working

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
+    quickshell.url = "github:quickshell-mirror/quickshell";
+    ambxst = {
+      url = "github:axenide/ambxst";
+      flake = false;
+    };
   };
 
   outputs = inputs:
---
+++ modules/packages/ambsxt.nix
@@ -0,0 +1,52 @@
+# Though i didn't copy the code directly the code's structure is inspired from rexi's kurukuru bar https://github.com/Rexcrazy804/Zaphkiel/blob/master/pkgs/kurukurubar.nix
+
+{ inputs, ... }: {
+  perSystem = { pkgs, lib, self', inputs', ... }: 
+    let
+      quickshellPkg = inputs'.quickshell.packages.default;
+
+      fontconfig = pkgs.makeFontsConf {
+        fontDirectories = [
+          self'.packages.phosphor
+        ];
+      };
+
+      qsConfig = pkgs.stdenvNoCC.mkDerivation {
+        name = "ambxst-config";
+        src = inputs.ambxst;
+        installPhase = ''
+          mkdir -p $out
+          cp -r ./. $out
+        '';
+      };
+
+      qmlPath = lib.makeSearchPath "lib/qt-6/qml" [
+        pkgs.kdePackages.qtbase
+        pkgs.kdePackages.qtdeclarative
+        pkgs.kdePackages.qtmultimedia
+        pkgs.kdePackages.syntax-highlighting
+      ];
+
+    in {
+      packages.ambxst = pkgs.symlinkJoin {
+        pname = "ambxst";
+        version = "0.1.0"; 
+        paths = [ quickshellPkg pkgs.rembg ]; 
+
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+        postBuild = ''
+          makeWrapper ${lib.getExe quickshellPkg} $out/bin/ambxst \
+            --set FONTCONFIG_FILE "${fontconfig}" \
+            --set QML2_IMPORT_PATH "${qmlPath}" \
+            --add-flags "-p ${qsConfig}" \
+            --prefix PATH : "$out/bin"
+          rm $out/bin/quickshell
+        '';
+
+        meta.mainProgram = "ambxst";
+      };
+
+      packages.default = self'.packages.ambxst;
+    };
+}
